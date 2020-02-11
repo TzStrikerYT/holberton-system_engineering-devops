af334059c5a9159eb56cd8d7847b94a2e333c6da
TzStrikerYT <maicol.asus@gmail.com>
2020-02-10 23:54:29 -0500

    [add] add zombie task

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * eternal - infinite loop
+ *
+ * Return: 0
+ */
+int eternal(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i = 0;
+	pid_t son;
+	for (; i < 5; i++)
+	{
+		son = fork();
+		if (son == 0)
+		{
+			dprintf(1, "Zombie process created, PID: %d\n",
+				getpid());
+			return (0);
+		}
+	}
+	eternal();
+	return (0);
+}
