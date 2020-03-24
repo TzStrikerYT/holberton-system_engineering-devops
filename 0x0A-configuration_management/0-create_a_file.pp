d1891272e686f29bf2efe5a9766526e4a8461030
Michael <maicol.asus@gmail.com>
2020-03-24 06:54:01 +0000

    [fix] fix syntax

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #creates a file
-
-file {'/tmp/holberton':
+file { '/tmp/holberton':
   ensure  => 'file',
-  mode    => 0744,
-  owner   => www-data,
-  group   => www-data,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
