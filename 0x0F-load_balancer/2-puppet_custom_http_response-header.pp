c84d650e95438d672e3f5e19a0b3250b3afd627f
Michael <maicol.asus@gmail.com>
2020-04-04 03:58:12 +0000

    [add] add nwe line

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,5 +2,5 @@
 exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
-exec { '/usr/bin/env sudo sed -i "26i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf' }
-exec { '/usr/bin/env service nginx start': }
+exec {'/usr/bin/env sudo sed -i "26i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf': }
+exec {'/usr/bin/env service nginx start': }
