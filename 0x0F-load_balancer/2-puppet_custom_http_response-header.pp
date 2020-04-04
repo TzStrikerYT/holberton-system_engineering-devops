02c7001ea68643a5db0c424b47ac913ec83c2c5a
Michael <maicol.asus@gmail.com>
2020-04-04 03:39:21 +0000

    [fix] add holberton school

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,7 @@
 #install nginx
 exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
+exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
 
 -> file_line { "Add_header" :
    ensure => present,
