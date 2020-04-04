03bc829f1f1b26d8a37bd6d5bf4118793db5a1ce
Michael <maicol.asus@gmail.com>
2020-04-04 03:46:20 +0000

    [fix] fix line after write

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,10 +4,10 @@ exec {'/usr/bin/env apt-get -y install nginx': }
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
 
 -> file_line { "Add_header" :
-   ensure => present,
-   path   => '/etc/nginx/sites-available/default',
-   line   => "\tadd_header X-Served-By ${hostname};",
-   after  => 'server_name _;',
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 }
 
 exec { '/usr/bin/env service nginx start': }
