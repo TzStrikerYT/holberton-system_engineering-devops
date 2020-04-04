78eb361561c76adc78922d5751f65f99cd6454f7
Michael <maicol.asus@gmail.com>
2020-04-04 04:45:19 +0000

    [fix] fix new logic

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,17 +4,20 @@ exec { 'Update':
   provider => shell,
 }
 
-exec { 'Instaler':
+exec { 'Installer':
+  require  => Exec['Update'],
   command  => '/usr/bin/env sudo apt-get -y install nginx',
   provider => shell,
 }
 
 exec { 'add_http':
+  require  => Exec['Installer'],
   command  => '/usr/bin/env sudo sed -i "35i\     add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
 exec { 'start_nginx':
+  require  => Exec['add_http'],
   command  => '/usr/bin/env sudo service nginx start',
   provider => shell,
 }
