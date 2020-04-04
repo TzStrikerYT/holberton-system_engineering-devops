6a04975d4a8871fe59acf4bb39b76e3e22885a71
Michael <maicol.asus@gmail.com>
2020-04-04 03:34:23 +0000

    [fix] fix error couts

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,9 +4,9 @@ exec {'/usr/bin/env apt-get -y install nginx': }
 
 -> file_line { "Add_header" :
    ensure => present,
-   path   => "/etc/nginx/sites-available/default",
+   path   => '/etc/nginx/sites-available/default',
    line   => "\tadd_header X-Served-By ${hostname};",
-   after  => "server_name_;",
+   after  => 'server_name_;',
 }
 
 exec { '/usr/bin/env service nginx start': }
