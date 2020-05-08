b4531fd33c435a7b2396fee648a16ddcdd7707cb
Michael <maicol.asus@gmail.com>
2020-05-08 04:02:38 +0000

    [add] add task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,14 +5,14 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=""):
     """ prints first 10 top hot posts """
-    url = "https://api.reddit.com/r/{}/hot.json?after={}".format(
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
         subreddit, after)
     header = {'User-agent': 'anything'}
     request = requests.get(url, headers=header, allow_redirects=False)
 
     if request:
-        js = request.json()
-        child = js.get("data").data.get("children")
+        js = request.json().get("data")
+        child = js.get("children")
         [hot_list.append(i.get("data").get("title")) for i in child]
         after = js.get("after")
 
