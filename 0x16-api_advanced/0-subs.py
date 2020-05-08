6417f400e65fc2c419a5c9d1d82f20cd86d31468
Michael <maicol.asus@gmail.com>
2020-05-08 02:17:19 +0000

    [add] add task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" make requests """
 import requests
 
 
@@ -6,13 +7,20 @@ def number_of_subscribers(subreddit):
     """ request number of suscribers for a subreddit """
     url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
     header = {'User-agent': 'anything'}
+    request = requests.get(url, headers=header, allow_redirects=False)
+
+    if request.status_code != 200:
+        return 0
+
     try:
-        js = requests.get(url, headers=header, allow_redirects=False).json()
-    except ValueError:
-        print("Not Valid JSON")
+        js = request.json()
+    except:
         return 0
 
     result = js.get("data")
-    subs = result.get("subscribers")
 
-    return subs
+    if result:
+        subs = result.get("subscribers")
+        if subs:
+            return subs
+    return 0
