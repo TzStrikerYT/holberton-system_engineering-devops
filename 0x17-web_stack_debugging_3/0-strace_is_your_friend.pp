b6be1feca61eb32b49ee200d13a858289486ef41
TzStrikerYT <maicol.asus@gmail.com>
2020-05-20 22:20:54 -0500

    [add] add unique task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+#chage name of the file to fix error 500
+exec { '/bin/bash':
+  command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/ht\
+ml/wp-includes/class-wp-locale.phpp',
+  path    => ["/usr/bin", "/usr/sbin"],
+}
+
+service { 'apache2':
+  ensure  => running,
+  restart => 'sudo service apache2 start',
+}
