c486ea15c67509419a7ef5567e2dbe97f274297d
TzStrikerYT <maicol.asus@gmail.com>
2020-05-20 22:26:30 -0500

    [add] add unique task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 #chage name of the file to fix error 500
 exec { '/bin/bash':
-  command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/ht\
-ml/wp-includes/class-wp-locale.phpp',
+  command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   creates => "sudo mv /var/www/html/wp-includes/class-wp-locale.phpp",
   path    => ["/usr/bin", "/usr/sbin"],
 }
