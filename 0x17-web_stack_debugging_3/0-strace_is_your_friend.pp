c35b39eb2e0d2febc29ef3b85d4954041627d3bb
TzStrikerYT <maicol.asus@gmail.com>
2020-05-20 22:43:11 -0500

    [add] add unique task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 exec { '/bin/bash':
   command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
-  path    => ["/usr/bin", "/usr/sbin"],
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
 service { 'apache2':
