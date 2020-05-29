a26f424e5adb5e08f4592fe3fdb3c179891d198c
TzStrikerYT <maicol.asus@gmail.com>
2020-05-28 22:35:49 -0500

    [add] add mandatory task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-#puppet change value
+# puppet change value
 exec {'/usr/bin/env': 
   command => 'sed -i "s/15/1000/" /etc/default/nginx ; sudo service nginx restart',
   path    => ['/usr/bin', '/bin'],
